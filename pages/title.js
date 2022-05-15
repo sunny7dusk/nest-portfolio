f351bb884b62f76defbf20a7f6b119055a96c142
sunny7dusk <nat233947@gmail.com>
2022-05-14 23:53:07 -0400

    remove a console log oops

--- pages/title.js
+++ pages/title.js
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 
 export default function Title({ y }) {
-  useEffect(() => {
-    console.log(y);
-  }, [y]);
+  // useEffect(() => {
+  //   console.log(y);
+  // }, [y]);
   // const [y, setY] = useState(0);
 
   // useEffect(() => {
