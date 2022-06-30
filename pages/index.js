fffc0da3e3748a20e2560d11d5b6b3c5e7aa2db1
sunny7dusk <nat233947@gmail.com>
2022-06-29 21:37:03 -0400

    Added spotify links

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import Head from 'next/head';
-import Title from './title';
-import Intro from './intro';
-import Skills from './skills';
-import Projects from './project';
-import Bio from './bio';
-import Contact from './contacts';
-import Blogs from './blogs';
+import Head from "next/head";
+import Title from "./title";
+import Intro from "./intro";
+import Skills from "./skills";
+import Projects from "./project";
+import Bio from "./bio";
+import Contact from "./contacts";
+import Blogs from "./blogs";
 
-import { useEffect, useState } from 'react';
-import { motion, useViewportScroll } from 'framer-motion';
+import { useEffect, useState } from "react";
+import { motion, useViewportScroll } from "framer-motion";
+
+import SpotifyFavSong from "./spotify";
 
 export default function Home() {
   const { scrollYProgress } = useViewportScroll();
@@ -17,6 +19,37 @@ export default function Home() {
   useEffect(() => {
     scrollYProgress.onChange((v) => setY(v));
   }, [scrollYProgress]);
+
+  // const [vantaEffect, setVantaEffect] = useState(0);
+  // const myRef = useRef(null);
+  // useEffect(() => {
+  //   if (!vantaEffect) {
+  //     setVantaEffect(
+  //       NET({
+  //         el: myRef.current,
+  //         scale: 1.0,
+  //         scaleMobile: 1.0,
+  //         color: 0xcbd5e1,
+  //         backgroundColor: 0x171a26,
+  //       })
+  //     );
+  //   }
+  //   return () => {
+  //     if (vantaEffect) vantaEffect.destroy();
+  //   };
+  // }, [vantaEffect]);
+
+  // const vantaBody = {
+  //   el: 'body',
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.0,
+  //   minWidth: 200.0,
+  //   scale: 1.0,
+  //   scaleMobile: 1.0,
+  // };
+
   // const [scrollYProgress, setScrollYProgress] = useState(0);
   // const [prevY, setPrevY] = useState(0);
 
@@ -45,58 +78,59 @@ export default function Home() {
   //   };
   // }, []);
 
-  const previewImg = 'https://i.imgur.com/YWr7FcG.jpg';
+  const previewImg = "https://i.imgur.com/YWr7FcG.jpg";
 
   return (
     <>
       <Head>
         <title>Nate's Portfolio</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
         <meta
-          name='description'
-          content='Nathaniel Chai Zhuo En | Computer Science senior at Virginia Tech. Welcome to my portfolio! This is build with NextJS, Sanity.io and Tailwind.'
+          name="description"
+          content="Nathaniel Chai Zhuo En | Computer Science senior at Virginia Tech. Welcome to my portfolio! This is build with NextJS, Sanity.io and Tailwind."
         />
-        <link rel='canonical' href='/Portfolio' />
+        <link rel="canonical" href="/Portfolio" />
         <meta
-          property='og:title'
-          content='Nathaniel Chai Zhuo En | Portfolio'
+          property="og:title"
+          content="Nathaniel Chai Zhuo En | Portfolio"
         />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://www.sunny7dusk.dev/' />
-        <meta property='og:image' content={previewImg} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.sunny7dusk.dev/" />
+        <meta property="og:image" content={previewImg} />
         <meta
-          name='twitter:title'
-          content='Nathaniel Chai Zhuo En | Portfolio'
-        />
-        <meta
-          name='twitter:description'
-          content='Nathaniel Chai Zhuo En | Computer Science senior at Virginia Tech. Welcome to my portfolio! This is build with NextJS, Sanity.io and Tailwind.'
+          name="twitter:title"
+          content="Nathaniel Chai Zhuo En | Portfolio"
         />
         <meta
-          property='og:description'
-          content='Nathaniel Chai Zhuo En | Computer Science senior at Virginia Tech. Welcome to my portfolio! This is build with NextJS, Sanity.io and Tailwind.'
+          name="twitter:description"
+          content="Nathaniel Chai Zhuo En | Computer Science senior at Virginia Tech. Welcome to my portfolio! This is build with NextJS, Sanity.io and Tailwind."
         />
-        <meta name='twitter:image' content={previewImg} />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta property='image' content={previewImg} />
+        <meta
+          property="og:description"
+          content="Nathaniel Chai Zhuo En | Computer Science senior at Virginia Tech. Welcome to my portfolio! This is build with NextJS, Sanity.io and Tailwind."
+        />
+        <meta name="twitter:image" content={previewImg} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="image" content={previewImg} />
         <script
-          src='https://unpkg.com/github-devprofile@2/dist/card.component.min.mjs'
-          type='module'
+          src="https://unpkg.com/github-devprofile@2/dist/card.component.min.mjs"
+          type="module"
         ></script>
 
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Inter&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+          rel="stylesheet"
         />
       </Head>
-      <div className='max-h-[100%] h-[100%] w-[100vw] bg-[#171A26] scroll-smooth absolute'>
-        <div className='relative max-h-[100%] h-[100%]'>
+      {/* bg-[#171A26]  */}
+      <div className="max-h-[100%] h-[100%] w-[100vw] scroll-smooth absolute ">
+        <div className="relative max-h-[100%] h-[100%]">
           <div
             className={`fixed w-full h-[100%]`}
             // {y !== 0 ? 100 : 90}
@@ -108,57 +142,71 @@ export default function Home() {
             }}
           >
             <img
-              src={'/assets/dark7storm_full.webp'}
+              src={"/assets/dark7storm_full.webp"}
               width={1980}
               height={1080}
-              className='object-cover h-[100%] w-[100vw] object-[65%]'
+              className="object-cover h-[100%] w-[100vw] object-[65%]"
             />
-            <div className='h-[100%] bg-black opacity-25'></div>
+            <div className="h-[100%] bg-black opacity-25"></div>
           </div>
           <Title y={y} prevY={y} />
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-            animate={{ scale: 0.8 }}
-            transition={{ type: 'spring', ease: 'easeInOut' }}
-            className='w-full flex flex-col bg-[#171A26] justify-center'
-          >
-            <Intro />
-          </motion.div>
-          <Skills />
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-            animate={{ scale: 0.8 }}
-            transition={{ type: 'spring', ease: 'easeInOut' }}
-            className='w-full  flex flex-col justify-center align-middle mt-36 text-justify'
-          >
-            <Bio y={y} />
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-            animate={{ scale: 0.8 }}
-            transition={{ type: 'spring', ease: 'easeInOut' }}
-            className='w-full flex flex-col justify-center mt-36 text-justify align-middle'
-          >
-            <Projects />
-          </motion.div>
+          {/* ref={myRef} */}
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }}
+              animate={{ scale: 0.8 }}
+              transition={{ type: "spring", ease: "easeInOut" }}
+              className="w-full flex flex-col justify-center"
+            >
+              <Intro />
+            </motion.div>
+            <Skills />
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }}
+              animate={{ scale: 0.8 }}
+              transition={{ type: "spring", ease: "easeInOut" }}
+              className="w-full  flex flex-col justify-center align-middle mt-36 text-justify"
+            >
+              <Bio y={y} />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }}
+              animate={{ scale: 0.8 }}
+              transition={{ type: "spring", ease: "easeInOut" }}
+              className="w-full flex flex-col justify-center mt-36 text-justify align-middle"
+            >
+              <Projects />
+            </motion.div>
 
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ type: 'spring', ease: 'easeInOut' }}
-            className='w-full flex flex-col justify-center mt-36 text-justify align-middle'
-          >
-            <Blogs />
-          </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ type: "spring", ease: "easeInOut" }}
+              className="w-full flex flex-col justify-center mt-36 text-justify align-middle"
+            >
+              <Blogs />
+            </motion.div>
 
-          <Contact />
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }}
+              animate={{ scale: 0.8 }}
+              transition={{ type: "spring", ease: "easeInOut" }}
+              className="w-full flex flex-col justify-center align-middle mt-36 text-justify"
+            >
+              <SpotifyFavSong />
+            </motion.div>
+
+            <Contact />
+          </div>
         </div>
       </div>
     </>
--- pages/intro.js
+++ pages/intro.js
@@ -1,15 +1,13 @@
 export default function Intro({ y, prevY }) {
-  // console.log('y = ' + y);
-  // console.log('prevY = ' + prevY);
   return (
     <>
-      <div className='self-center w-[50vw]'>
-        <h1 className='ease-in-out duration-300 bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D] via-[#F2CC85] to-[#84B8D9] text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-6xl tracking-wide text-center'>
+      <div className="self-center w-[50vw] ">
+        <h1 className="ease-in-out duration-300 bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D] via-[#F2CC85] to-[#84B8D9] text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-6xl tracking-wide text-center">
           Nathaniel Chai Zhuo En 蔡卓恩
         </h1>
         <br />
-        <div className='text-justify'>
-          <span className='bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-sm sm:text-1xl lg:text-2xl 2xl:text-5xl'>
+        <div className="text-justify">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-sm sm:text-1xl lg:text-2xl 2xl:text-5xl">
             Growing up in Malaysia, I have always been fascinated by the
             technological side of the world. Driven by the desire to learn, I
             constantly immerse myself in new and upcoming programming
@@ -18,12 +16,6 @@ export default function Intro({ y, prevY }) {
           </span>
         </div>
       </div>
-      {/* <div
-        // style={{ opacity: `${y >= 1000 ? 1 - (y - 1000) / 100 : 1}` }}
-        // className='w-full flex flex-col bg-[#171A26] justify-center'
-      >
-        
-      </div> */}
     </>
   );
 }
