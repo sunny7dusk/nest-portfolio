f97d1ffc7ac095764477a4ad242d9b0a0713c1a4
sunny7dusk <nat233947@gmail.com>
2022-08-02 12:41:57 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,28 @@ export default function Home() {
       {/* bg-[#171A26]  */}
       <div className="max-h-[100%] h-[100%] w-[100vw] scroll-smooth absolute">
         <div className="w-full h-[100%] relative">
-          <Title y={y} />
+          <motion.div
+            transition={{ type: "spring", ease: "easeInOut" }}
+            style={{
+              opacity: `${1 - y / 400}`,
+              translateX: `${y}px`,
+            }}
+            className={`mb-10 fixed snap-center w-full h-[100%] overflow-hidden flex flex-col lg:px-[4rem] px-6 sm:px-10 align-top justify-center animate ease-in-out `}
+          >
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D]  to-[#84B8D9] text-lg sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-9xl tracking-wide">
+              FULL STACK <br /> DEVELOPER
+            </span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-sm sm:text-3xl lg:text-4xl 2xl:text-7xl">
+              Nate
+            </span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
+              Computer Science
+            </span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
+              Virginia Tech
+            </span>
+          </motion.div>
+
           <motion.div
             // initial={{ opacity: 0 }}
             // whileInView={{ opacity: 1 }}
--- pages/title.js
+++ pages/title.js
@@ -1,13 +1,7 @@
-export default function Title({ y }) {
+export default function Title() {
   return (
     <>
-      <div
-        style={{
-          opacity: `${1 - y / 400}`,
-          translate: `${y}px`,
-        }}
-        className={`mb-10 fixed snap-center w-full h-[100%] overflow-hidden flex flex-col lg:px-[4rem] px-6 sm:px-10 align-top justify-center animate ease-in-out `}
-      >
+      <div>
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D]  to-[#84B8D9] text-lg sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-9xl tracking-wide">
           FULL STACK <br /> DEVELOPER
         </span>
