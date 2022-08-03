322f8def118edd8a0677ad30f72419978290020f
sunny7dusk <nat233947@gmail.com>
2022-08-02 21:18:59 -0400

    more improvements

--- pages/index.js
+++ pages/index.js
@@ -82,91 +82,48 @@ export default function Home() {
       </Head>
       {/* bg-[#171A26]  */}
       <div className="max-h-[100%] h-[100%] w-[100vw] scroll-smooth absolute">
-        {currInnerWidth >= 640 ? (
-          <div className="w-full h-[100%] relative">
-            <motion.div
-              transition={{ type: "spring", ease: "easeInOut" }}
-              style={{
-                opacity: `${1 - y / 400}`,
-              }}
-              className={`mb-10 fixed snap-center w-full h-[100%] overflow-hidden flex flex-col lg:px-[4rem] px-6 sm:px-10 align-top justify-center animate ease-in-out `}
-            >
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D]  to-[#84B8D9] text-lg sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-9xl tracking-wide">
-                FULL STACK <br /> DEVELOPER
-              </span>
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-sm sm:text-3xl lg:text-4xl 2xl:text-7xl">
-                Nate
-              </span>
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
-                Computer Science
-              </span>
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
-                Virginia Tech
-              </span>
-            </motion.div>
+        <div className="w-full h-[100%] relative">
+          <motion.div
+            transition={{ type: "spring", ease: "easeInOut" }}
+            style={{
+              opacity: `${1 - y / 400}`,
+              translateX: `${y}px`,
+            }}
+            className={`mb-10 fixed snap-center w-full h-[100%] overflow-hidden flex flex-col lg:px-[4rem] px-6 sm:px-10 align-top justify-center animate ease-in-out `}
+          >
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D]  to-[#84B8D9] text-lg sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-9xl tracking-wide">
+              FULL STACK <br /> DEVELOPER
+            </span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-sm sm:text-3xl lg:text-4xl 2xl:text-7xl">
+              Nate
+            </span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
+              Computer Science
+            </span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
+              Virginia Tech
+            </span>
+          </motion.div>
 
-            <motion.div
-              transition={{ type: "spring", ease: "easeInOut", delay: 0.25 }}
-              className={`w-full fixed h-[100%] overflow-hidden`}
-              style={{
-                clipPath: `polygon(${90}% 0, 100% 0%, 100% 100%, ${0}% 100%)`,
-                opacity: `${1 - y / 100}`,
-                translateX: `${currInnerWidth >= 640 ? y : 0}px`,
-                translateY: `${currInnerWidth >= 640 ? -y : 0}px`,
-              }}
-            >
-              <img
-                src={"/assets/dark7storm_full.webp"}
-                width={1980}
-                height={1080}
-                className="object-cover h-[100%] w-[100vw] object-[65%]"
-              />
-              {/* <div className="h-[100%] bg-black opacity-25"></div> */}
-            </motion.div>
-          </div>
-        ) : (
-          y <= 200 && (
-            <div className="w-full h-[100%] relative">
-              <motion.div
-                transition={{ type: "spring", ease: "easeInOut" }}
-                style={{
-                  opacity: `${1 - y / 400}`,
-                }}
-                className={`mb-10 fixed snap-center w-full h-[100%] overflow-hidden flex flex-col lg:px-[4rem] px-6 sm:px-10 align-top justify-center animate ease-in-out `}
-              >
-                <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D]  to-[#84B8D9] text-lg sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-9xl tracking-wide">
-                  FULL STACK <br /> DEVELOPER
-                </span>
-                <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-sm sm:text-3xl lg:text-4xl 2xl:text-7xl">
-                  Nate
-                </span>
-                <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
-                  Computer Science
-                </span>
-                <span className="bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-400 text-xs sm:text-xl lg:text-2xl 2xl:text-4xl">
-                  Virginia Tech
-                </span>
-              </motion.div>
-
-              <motion.div
-                transition={{ type: "spring", ease: "easeInOut", delay: 0.25 }}
-                className={`w-full fixed h-[100%] overflow-hidden`}
-                style={{
-                  clipPath: `polygon(${90}% 0, 100% 0%, 100% 100%, ${0}% 100%)`,
-                  opacity: `${1 - y / 100}`,
-                }}
-              >
-                <img
-                  src={"/assets/dark7storm_full.webp"}
-                  width={1980}
-                  height={1080}
-                  className="object-cover h-[100%] w-[100vw] object-[65%]"
-                />
-                {/* <div className="h-[100%] bg-black opacity-25"></div> */}
-              </motion.div>
-            </div>
-          )
-        )}
+          <motion.div
+            transition={{ type: "spring", ease: "easeInOut", delay: 0.25 }}
+            className={`w-full fixed h-[100%] overflow-hidden`}
+            style={{
+              clipPath: `polygon(${90}% 0, 100% 0%, 100% 100%, ${0}% 100%)`,
+              opacity: `${1 - y / 100}`,
+              translateX: `${y}px`,
+              translateY: `${-y}px`,
+            }}
+          >
+            <img
+              src={"/assets/dark7storm_full.webp"}
+              width={1980}
+              height={1080}
+              className="object-cover h-[100%] w-[100vw] object-[65%]"
+            />
+            {/* <div className="h-[100%] bg-black opacity-25"></div> */}
+          </motion.div>
+        </div>
 
         <Player
           autoplay
