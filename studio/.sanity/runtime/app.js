64061e945b3be90654ef336f4e332ab45f0a659d
sunny7dusk <nat233947@gmail.com>
2022-08-03 01:24:21 -0400

    improvements

---
+++ studio/.sanity/runtime/app.js
@@ -0,0 +1,10 @@
+
+// This file is auto-generated on 'sanity start'
+// Modifications to this file is automatically discarded
+import {renderStudio} from "sanity"
+import studioConfig from "../../node_modules/sanity/node_modules/@sanity/server/lib/defaultStudioConfig.js"
+
+renderStudio(
+  document.getElementById("sanity"),
+  studioConfig
+)
