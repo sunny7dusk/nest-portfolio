782245f95624104a0c1f012388e85ef65e0ce2bd
sunny7dusk <nat233947@gmail.com>
2022-08-06 15:39:47 -0400

    fixed name

--- pages/intro.js
+++ pages/intro.js
@@ -1,21 +1,21 @@
 export default function Intro() {
   return (
     <>
-      <header className="self-center w-[70vw] sm:w-[50vw] ">
-        <h1 className="ease-in-out duration-300 bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D] via-[#F2CC85] to-[#84B8D9] text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-6xl tracking-wide text-center">
+      <header>
+        <h1 className="ease-in-out duration-300 bg-clip-text text-transparent bg-gradient-to-r from-[#A3767D] via-[#F2CC85] to-[#84B8D9] text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-6xl tracking-wide text-center w-[100vw]">
           Nathaniel Chai Zhuo En 蔡卓恩
         </h1>
         <br />
-        <div className="text-justify">
-          <span className="text-slate-300 text-md sm:text-xl lg:text-2xl 2xl:text-5xl">
-            Growing up in Malaysia, I have always been fascinated by the
-            technological side of the world. Driven by the desire to learn, I
-            constantly immerse myself in new and upcoming programming
-            technologies. This is what led me here today, as a programmer
-            studying Computer Science at Virginia Tech.
-          </span>
-        </div>
       </header>
+      <div className="text-justify self-center w-[70vw] sm:w-[50vw] ">
+        <span className="text-slate-300 text-md sm:text-xl lg:text-2xl 2xl:text-5xl">
+          Growing up in Malaysia, I have always been fascinated by the
+          technological side of the world. Driven by the desire to learn, I
+          constantly immerse myself in new and upcoming programming
+          technologies. This is what led me here today, as a programmer studying
+          Computer Science at Virginia Tech.
+        </span>
+      </div>
     </>
   );
 }
