fa018d1cdafb984ed5c4c509eb9fe0b367e8c9e0
sunny7dusk <nat233947@gmail.com>
2022-08-07 10:23:37 -0400

    fixing text issues with bio

--- pages/bio.js
+++ pages/bio.js
@@ -35,22 +35,20 @@ export default function Bio() {
             {" "}
             2019 (Ongoing)
           </time>
-          <div className="pt-2 pl-2 sm:pt-4 sm:pl-4">
+          <div className="pt-2 pl-2 pb-2 sm:pt-4 sm:pl-4 sm:pb-4">
             Studying Computer Science at{" "}
+            <nav aria-label="computer science at virginia tech">
+              <a
+                href="https://cs.vt.edu/"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="underline"
+              >
+                Virginia Tech
+              </a>{" "}
+              as a Senior
+            </nav>
           </div>
-          <nav
-            className="pb-2 pl-2 sm:pb-4 sm:pl-4"
-            aria-label="computer science at virginia tech"
-          >
-            <a
-              href="https://cs.vt.edu/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="underline"
-            >
-              Virginia Tech (Senior)
-            </a>{" "}
-          </nav>
         </li>
         {/* <FaCaretDown color="#94a3b8" className="w-full justify-end text-end" /> */}
         <li className="hover:text-white animation ease-in-out duration-100 list-none border-l-2 border-slate-400 pb-4">
@@ -58,22 +56,19 @@ export default function Bio() {
           <time className="p-2 sm:p-4 text-xs sm:text-md lg:text-xl 2xl:text-4xl">
             2021 (Ongoing)
           </time>
-          <div className="pt-2 pl-2 sm:pt-4 sm:pl-4">
+          <div className="pt-2 pl-2 pb-2 sm:pt-4 sm:pl-4 sm:pb-4">
             Social Media and Communications Lead,{" "}
+            <nav aria-label="google developer student clubs at virginia tech">
+              <a
+                href="https://sites.google.com/vt.edu/dscvt"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="underline"
+              >
+                Google Developer Student Club at Virginia Tech
+              </a>
+            </nav>
           </div>
-          <nav
-            className="pb-2 pl-2 sm:pb-4 sm:pl-4"
-            aria-label="google developer student clubs at virginia tech"
-          >
-            <a
-              href="https://sites.google.com/vt.edu/dscvt"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="underline"
-            >
-              Google Developer Student Club at Virginia Tech
-            </a>
-          </nav>
         </li>
         {/* <FaCaretDown color="#94a3b8" className="w-full justify-end text-end" /> */}
         <li className="hover:text-white animation ease-in-out duration-100 list-none border-l-2 border-slate-400 pb-4">
@@ -81,22 +76,19 @@ export default function Bio() {
           <time className="p-2 sm:p-4 text-xs sm:text-md lg:text-xl 2xl:text-4xl">
             2021 (Ongoing)
           </time>
-          <div className="pt-2 pl-2 sm:pt-4 sm:pl-4">
+          <div className="pt-2 pl-2 pb-2 sm:pt-4 sm:pl-4 sm:pb-4">
             Web Development team,{" "}
+            <nav aria-label="rocketry at virginia tech">
+              <a
+                href="https://www.rocketryatvirginiatech.org/"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="underline"
+              >
+                Rocketry at Virginia Tech
+              </a>
+            </nav>
           </div>
-          <nav
-            className="pb-2 pl-2 sm:pb-4 sm:pl-4"
-            aria-label="rocketry at virginia tech"
-          >
-            <a
-              href="https://www.rocketryatvirginiatech.org/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="underline"
-            >
-              Rocketry at Virginia Tech
-            </a>
-          </nav>
         </li>
         {/* <FaCaretDown color="#94a3b8" className="w-full justify-end text-end" /> */}
         <li className="hover:text-white animation ease-in-out duration-100 list-none border-l-2 border-slate-400 pb-4">
@@ -105,19 +97,19 @@ export default function Bio() {
           <time className="p-2 sm:p-4 text-xs sm:text-md lg:text-xl 2xl:text-4xl">
             2022
           </time>
-          <div className="pt-2 pl-2 sm:pt-4 sm:pl-4">
-            Software Engineering Intern (React components team),{" "}
+          <div className="pt-2 pl-2 pb-2 sm:pt-4 sm:pl-4 sm:pb-4">
+            Software Engineering Intern (React components team),
+            <nav aria-label="sciencelogic">
+              <a
+                href="https://sciencelogic.com/"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="underline"
+              >
+                ScienceLogic
+              </a>
+            </nav>
           </div>
-          <nav className="pb-2 pl-2 sm:pb-4 sm:pl-4" aria-label="sciencelogic">
-            <a
-              href="https://sciencelogic.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="underline"
-            >
-              ScienceLogic
-            </a>
-          </nav>
         </li>
       </ul>
     </>
