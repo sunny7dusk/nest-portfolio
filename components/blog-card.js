ff81acb2744c94019ceb8c78b5cb006961c714bb
sunny7dusk <nat233947@gmail.com>
2022-08-28 16:03:33 -0400

    fixed changes

--- components/blog-card.js
+++ components/blog-card.js
@@ -23,8 +23,8 @@ function SmallVersion({ item }) {
               {item.publishedAt.split("T")[0]}
             </p>
           )}
-          <h5 className="mt-2 text-xl font-bold text-white">{item.title}</h5>
-          <div className="flex items-center justify-between mt-6">
+          <h5 className="mt-4 text-xl font-bold text-white">{item.title}</h5>
+          <div className="flex items-center justify-between mt-2">
             <p className="text-lg font-medium text-transparent bg-clip-text bg-gradient-to-r from-[#A3767D] via-[#F2CC85] to-[#84B8D9]">
               {item.categories[0].title}
             </p>
@@ -38,7 +38,6 @@ function SmallVersion({ item }) {
 function LargeVersion({ item }) {
   return (
     <Link href={`/blog/${item.slug.current}`}>
-      {" "}
       <div className="flex flex-col justify-end h-full p-6 bg-gray-900 sm:p-8 rounded-xl hover:bg-opacity-90">
         {item.imageUrl && (
           <img
@@ -53,8 +52,8 @@ function LargeVersion({ item }) {
               {item.publishedAt.split("T")[0]}
             </p>
           )}
-          <h5 className="mt-2 text-xl font-bold text-white">{item.title}</h5>
-          <div className="flex items-center justify-between mt-6">
+          <h5 className="mt-4 text-xl font-bold text-white">{item.title}</h5>
+          <div className="flex items-center justify-between mt-2">
             <p className="text-lg font-medium text-transparent bg-clip-text bg-gradient-to-r from-[#A3767D] via-[#F2CC85] to-[#84B8D9]">
               {item.categories[0].title}
             </p>
