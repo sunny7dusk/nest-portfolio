49866f6b1d17e71d0037fd690a34de231a277ec8
Nathaniel Chai <nat233947@gmail.com>
2025-03-01 17:14:09 -0500

    design updates

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 export default function NotFoundPage() {
   return (
-    <div className="flex flex-col justify-center items-center w-[100vw] h-full">
-      <span className="text-9xl">&#129335;</span>
+    <div className="flex flex-col justify-center items-center h-full">
+      <span className="text-6xl md:text-9xl">&#129335;</span>
     </div>
   );
 }
